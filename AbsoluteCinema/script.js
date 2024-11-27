b8022d8042ed6a93cbf69e6ccbd08ca0d0b7a739
PLucasDev <plucas.soares01@gmail.com>
2024-11-27 09:59:27 -0300

    Versão 2 - função para acessar o filme pelo nome e buscar os horarios referentes pronta!

--- AbsoluteCinema/script.js
+++ AbsoluteCinema/script.js
@@ -1,10 +1,50 @@
+const readlineSync = require('readline-sync');
+
 let filmes = [
-{nome: "Arcane", horarios:"10:00 á 14:00",nota:"10/10"},
-//
-{nome: "Dr House", horarios:"15:00 á 17:00",nota:"7,9"},
-//
-{nome: "The Walking Dead", horarios:"18:00 á 20:00",nota:"8,9"},
-//
-{nome: "Robo Selvagem", horarios:"20:00 á 00:00",nota:"9,0"}
+    {   nome: "• Arcane", nota: "10/10",
+        horarios: {
+            horario1: "10:00",
+            horario2: "11:00",
+            horario3: "12:00"
+        }},
+    //
+    {   nome: "• Dr House", nota: "7,9",
+        horarios: {
+            horario1: "10:00",
+            horario2: "11:00",
+            horario3: "12:00"
+        }},
+    //
+    {   nome: "• The Walking Dead", nota: "8,9",
+        horarios: {
+            horario1: "13:00",
+            horario2: "14:00",
+            horario3: "15:00"
+        }},
+    //
+    {   nome: "• Robo Selvagem", nota: "9,0",
+        horarios: {
+            horario1: "16:00",
+            horario2: "17:00",
+            horario3: "18:00"
+        }}
 ]
+
+let pergunta = readlineSync.question("Selecione um dos filmes que deseja assistir ");
+
+const filmeSelecionado = filmes.find(filme => filme.nome.includes(pergunta));
+
+if (filmeSelecionado) {
+    console.log(`Horarios Disponiveis para ${filmeSelecionado.nome}`);
+    console.log(filmeSelecionado.horarios.horario1);
+    console.log(filmeSelecionado.horarios.horario2);
+    console.log(filmeSelecionado.horarios.horario3);
+} else {console.log ("Filme não encontrado, tente novamente")}
+
+
+//  filmes.forEach((filmes) => {
+//     console.log(`O filme que você selecionou foi ${pergunta} com nota ${filmes.nota}`)
+// }) //Mostra os itens do array
+
+ 
 
---
+++ AbsoluteCinema/teste.js
@@ -0,0 +1,16 @@
+const readlineSync = require('readline-sync');
+
+/*
+var question = parseInt(readlineSync.question("Escolha um numero de 0 a 10 " )) ;
+
+if (question === 0) {
+    console.log("Você escolheu o numero ZERO")
+} else if (question <= 5) {
+    console.log("O numero que você escolheu é menor ou igual a CINCO")
+} else if (question <= 9) {
+    console.log("O numero que você escolheu é maior que CINCO")
+} else if ( question === 10) {
+    console.log("O numero que você escolheu é DEIXXXXXXXXXX")
+} else {
+    console.log("Escolha algum NUMERO valido")
+} */
